d75f92854de1c1e8d530402554f42b7bca4ee422
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2024-07-27 18:33:09 +0530

    Create triangle.c

---
+++ 27.07.2024/triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter sides of triangle: ");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a+b>c,a+c>b,b+c>a)
+	{
+		printf("Valid\n");
+	}
+	else
+	{
+		printf("Not valid\n");
+	}
+}
