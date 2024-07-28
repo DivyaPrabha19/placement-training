3fd870df716788e5ff050920fb1762cab600218e
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2024-07-28 19:42:17 +0530

    Create Average of numbers.c

---
+++ 28.07.2024/Average of numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float num, sum = 0.0, average;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        printf("Enter number %d: ", i);
+        scanf("%f", &num);
+        sum += num;
+    }
+
+    average = sum / n;
+    printf("Average = %.2f\n", average);
+    return 0;
+}
