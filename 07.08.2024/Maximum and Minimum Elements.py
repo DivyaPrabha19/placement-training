6752417a4a4baa6c6820a23ec7f51b804967dadd
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2024-08-07 22:53:30 +0530

    Create Maximum and Minimum Elements.py

---
+++ 07.08.2024/Maximum and Minimum Elements.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([10, 20, 5, 15, 30])
+max_element = np.max(arr)
+min_element = np.min(arr)
+print("Maximum element:", max_element)
+print("Minimum element:", min_element)
